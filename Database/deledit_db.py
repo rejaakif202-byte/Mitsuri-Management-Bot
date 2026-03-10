2e475576c0cddf75c520d2c2f24f60b812e54dfc
rejaakif202-byte <rejaakif202@gmail.com>
2026-03-10 18:35:12 +0530

    Update deledit_db.py

--- Database/deledit_db.py
+++ Database/deledit_db.py
@@ -1,4 +1,5 @@
 edit_db = {}
+approved_users = {}
 
 
 async def toggle_edit_delete(chat_id, state):
@@ -23,3 +24,30 @@ async def get_edit_settings(chat_id):
         edit_db[chat_id] = {"enabled": False, "timer": 5}
 
     return edit_db[chat_id]
+
+
+async def approve_user(chat_id, user_id):
+
+    if chat_id not in approved_users:
+        approved_users[chat_id] = []
+
+    if user_id not in approved_users[chat_id]:
+        approved_users[chat_id].append(user_id)
+
+
+async def unapprove_user(chat_id, user_id):
+
+    if chat_id in approved_users:
+
+        if user_id in approved_users[chat_id]:
+            approved_users[chat_id].remove(user_id)
+
+
+async def is_approved(chat_id, user_id):
+
+    if chat_id in approved_users:
+
+        if user_id in approved_users[chat_id]:
+            return True
+
+    return False
