2b8742be151d75f995923166bbd54b2e8328f96e
rejaakif202-byte <rejaakif202@gmail.com>
2026-03-11 05:00:54 +0530

    Update fullpromote.py

--- modules/admin/fullpromote.py
+++ modules/admin/fullpromote.py
@@ -1,7 +1,10 @@
 from pyrogram import Client, filters
 from pyrogram.types import Message
 
-OWNER_ID = 7846306818  # <-- apna id yaha daalna
+from utils.permissions import is_admin
+from database.promote_db import add_promoted
+
+OWNER_ID = 7846306818
 
 
 @Client.on_message(filters.command("fullpromote") & filters.group)
@@ -9,34 +12,22 @@ async def fullpromote(client: Client, message: Message):
 
     user = None
 
-    # reply method
     if message.reply_to_message:
         user = message.reply_to_message.from_user
 
-    # username / id
     elif len(message.command) > 1:
+
         try:
             user = await client.get_users(message.command[1])
         except:
             return await message.reply_text("User not found.")
 
-    # OWNER SELF PROMOTE FEATURE
     if message.from_user.id == OWNER_ID and message.reply_to_message:
         user = message.reply_to_message.from_user
 
     if not user:
         return await message.reply_text(
             "Reply to a user or give username/user id."
-        )
-
-    try:
-        member = await client.get_chat_member(
-            message.chat.id,
-            user.id
-        )
-    except:
-        return await message.reply_text(
-            "User is not in this group."
         )
 
     try:
@@ -56,6 +47,8 @@ async def fullpromote(client: Client, message: Message):
             can_promote_members=True
 
         )
+
+        await add_promoted(message.chat.id, user.id)
 
     except:
         return await message.reply_text(
