e1562f5b51302de13950400d3ab8fddd1043c5ce
rejaakif202-byte <rejaakif202@gmail.com>
2026-03-10 15:33:30 +0530

    Update permissions.py

--- Utils/permissions.py
+++ Utils/permissions.py
@@ -1,5 +1,8 @@
 async def is_admin(client, message):
 
+    if not message.from_user:
+        return False
+
     member = await client.get_chat_member(
         message.chat.id,
         message.from_user.id
