f4b0cfcd2083ec4c765315c2e1d6be86d929ef0a
rejaakif202-byte <rejaakif202@gmail.com>
2026-03-11 13:03:01 +0530

    Update approve.py

--- modules/admin/approve.py
+++ modules/admin/approve.py
@@ -1,10 +1,10 @@
 from pyrogram import Client, filters
 from pyrogram.types import Message
-from Utils.permissions import is_admin
-from Database.approve_db import (
+
+from utils.permissions import is_admin
+from database.approve_db import (
     approve_user,
     unapprove_user,
-    is_approved,
     approve_all,
     unapprove_all,
     get_approved_list
@@ -14,47 +14,53 @@ from Database.approve_db import (
 # ---------------- APPROVE ---------------- #
 
 @Client.on_message(filters.command("approve") & filters.group)
-async def approve(client, message: Message):
+async def approve(client: Client, message: Message):
 
     if not await is_admin(client, message):
         return
 
-    if message.reply_to_message:
-        user = message.reply_to_message.from_user
-    else:
+    if not message.reply_to_message:
         return await message.reply_text("<b>Reply to a user to approve</b>")
+
+    user = message.reply_to_message.from_user
+
+    if not user:
+        return
 
     await approve_user(message.chat.id, user.id)
 
     await message.reply_text(
-        f"<b>{user.mention} is now approved</b>"
+        f"<b>{user.mention} is now approved.</b>"
     )
 
 
 # ---------------- UNAPPROVE ---------------- #
 
 @Client.on_message(filters.command("unapprove") & filters.group)
-async def unapprove(client, message: Message):
+async def unapprove(client: Client, message: Message):
 
     if not await is_admin(client, message):
         return
 
-    if message.reply_to_message:
-        user = message.reply_to_message.from_user
-    else:
+    if not message.reply_to_message:
         return await message.reply_text("<b>Reply to a user to unapprove</b>")
+
+    user = message.reply_to_message.from_user
+
+    if not user:
+        return
 
     await unapprove_user(message.chat.id, user.id)
 
     await message.reply_text(
-        f"<b>{user.mention} is now unapproved</b>"
+        f"<b>{user.mention} is now unapproved.</b>"
     )
 
 
 # ---------------- APPROVE ALL ---------------- #
 
 @Client.on_message(filters.command("approveall") & filters.group)
-async def approveall(client, message: Message):
+async def approveall(client: Client, message: Message):
 
     if not await is_admin(client, message):
         return
@@ -62,14 +68,14 @@ async def approveall(client, message: Message):
     await approve_all(message.chat.id)
 
     await message.reply_text(
-        "<b>All members are now approved</b>"
+        "<b>All members are now approved.</b>"
     )
 
 
 # ---------------- UNAPPROVE ALL ---------------- #
 
 @Client.on_message(filters.command("unapproveall") & filters.group)
-async def unapproveall(client, message: Message):
+async def unapproveall(client: Client, message: Message):
 
     if not await is_admin(client, message):
         return
@@ -77,19 +83,19 @@ async def unapproveall(client, message: Message):
     await unapprove_all(message.chat.id)
 
     await message.reply_text(
-        "<b>All members are now unapproved</b>"
+        "<b>All members are now unapproved.</b>"
     )
 
 
 # ---------------- APPROVE LIST ---------------- #
 
 @Client.on_message(filters.command("approvelist") & filters.group)
-async def approvelist(client, message: Message):
+async def approvelist(client: Client, message: Message):
 
     users = await get_approved_list(message.chat.id)
 
     if not users:
-        return await message.reply_text("<b>No approved users</b>")
+        return await message.reply_text("<b>No approved users.</b>")
 
     text = "<b>Approved Users:</b>\n\n"
 
@@ -100,11 +106,11 @@ async def approvelist(client, message: Message):
         try:
             user = await client.get_users(user_id)
 
-            text += f"{count}. <b>{user.mention}</b> - <code>{user_id}</code>\n"
+            text += f"{count}. {user.mention} - <code>{user_id}</code>\n"
 
             count += 1
 
-        except:
+        except Exception:
             pass
 
     await message.reply_text(text)
