125abfcafc941d079c7755130f443ebfc1de0a94
rejaakif202-byte <rejaakif202@gmail.com>
2026-03-11 13:09:06 +0530

    Update permissions.py

--- Utils/permissions.py
+++ Utils/permissions.py
@@ -1,20 +1,73 @@
 from pyrogram.types import Message
 
 
+# ---------------- USER ADMIN CHECK ---------------- #
+
 async def is_admin(client, message: Message):
 
-    user = message.from_user
-
-    if not user:
+    if not message.from_user:
         return False
 
-    member = await client.get_chat_member(message.chat.id, user.id)
+    try:
+        member = await client.get_chat_member(
+            message.chat.id,
+            message.from_user.id
+        )
 
-    if member.status in ["administrator", "creator"]:
-        return True
+        if member.status in ["administrator", "creator"]:
+            return True
+
+    except:
+        pass
 
     await message.reply_text(
         "<b>You must be an admin to use this command.</b>"
+    )
+
+    return False
+
+
+# ---------------- OWNER CHECK ---------------- #
+
+async def is_owner(client, message: Message):
+
+    try:
+        member = await client.get_chat_member(
+            message.chat.id,
+            message.from_user.id
+        )
+
+        if member.status == "creator":
+            return True
+
+    except:
+        pass
+
+    await message.reply_text(
+        "<b>Only the group owner can use this command.</b>"
+    )
+
+    return False
+
+
+# ---------------- BOT ADMIN CHECK ---------------- #
+
+async def bot_is_admin(client, message: Message):
+
+    try:
+        bot = await client.get_chat_member(
+            message.chat.id,
+            "me"
+        )
+
+        if bot.status in ["administrator", "creator"]:
+            return True
+
+    except:
+        pass
+
+    await message.reply_text(
+        "<b>I need admin permissions to perform this action.</b>"
     )
 
     return False
