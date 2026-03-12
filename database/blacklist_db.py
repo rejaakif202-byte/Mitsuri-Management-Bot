b57f9e738eac065b2926a34121faa1e7aff2fc36
rejaakif202-byte <rejaakif202@gmail.com>
2026-03-13 05:13:43 +0530

    Update blacklist_db.py

--- database/blacklist_db.py
+++ database/blacklist_db.py
@@ -1,26 +1,49 @@
-blacklist_db = {}
+from motor.motor_asyncio import AsyncIOMotorClient
+from config import MONGO_URL, DATABASE_NAME
 
+mongo = AsyncIOMotorClient(MONGO_URL)
+db = mongo[DATABASE_NAME]
+
+blacklist = db["blacklist"]
+
+
+# ADD BLACKLIST WORD
 
 async def add_blacklist(chat_id, word):
 
-    if chat_id not in blacklist_db:
-        blacklist_db[chat_id] = []
+    data = await blacklist.find_one({"chat_id": chat_id})
 
-    if word not in blacklist_db[chat_id]:
-        blacklist_db[chat_id].append(word)
+    if not data:
+        await blacklist.insert_one({
+            "chat_id": chat_id,
+            "words": [word]
+        })
+        return
 
+    if word not in data["words"]:
+        await blacklist.update_one(
+            {"chat_id": chat_id},
+            {"$push": {"words": word}}
+        )
+
+
+# REMOVE BLACKLIST WORD
 
 async def remove_blacklist(chat_id, word):
 
-    if chat_id in blacklist_db:
+    await blacklist.update_one(
+        {"chat_id": chat_id},
+        {"$pull": {"words": word}}
+    )
 
-        if word in blacklist_db[chat_id]:
-            blacklist_db[chat_id].remove(word)
 
+# GET BLACKLIST WORDS
 
 async def get_blacklist(chat_id):
 
-    if chat_id not in blacklist_db:
+    data = await blacklist.find_one({"chat_id": chat_id})
+
+    if not data:
         return []
 
-    return blacklist_db[chat_id]
+    return data.get("words", [])
