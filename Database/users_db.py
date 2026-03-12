3637ea8103ab300967d096969277d3e1f193ecc8
rejaakif202-byte <rejaakif202@gmail.com>
2026-03-12 15:46:30 +0530

    Update users_db.py

--- Database/users_db.py
+++ Database/users_db.py
@@ -1,22 +1,62 @@
-users = set()
-chats = set()
+from motor.motor_asyncio import AsyncIOMotorClient
+from config import MONGO_URL, DATABASE_NAME
 
+client = AsyncIOMotorClient(MONGO_URL)
+db = client[DATABASE_NAME]
+
+users_collection = db.users
+chats_collection = db.chats
+
+
+# ADD USER
 
 async def add_user(user_id):
-    users.add(user_id)
 
+    user = await users_collection.find_one({"user_id": user_id})
+
+    if not user:
+        await users_collection.insert_one({"user_id": user_id})
+
+
+# ADD CHAT
 
 async def add_chat(chat_id):
-    chats.add(chat_id)
 
+    chat = await chats_collection.find_one({"chat_id": chat_id})
+
+    if not chat:
+        await chats_collection.insert_one({"chat_id": chat_id})
+
+
+# GET USERS LIST
 
 async def get_users():
-    return list(users)
 
+    users = []
+    async for user in users_collection.find():
+        users.append(user["user_id"])
+
+    return users
+
+
+# GET CHATS LIST
+
+async def get_chats():
+
+    chats = []
+    async for chat in chats_collection.find():
+        chats.append(chat["chat_id"])
+
+    return chats
+
+
+# TOTAL USERS
 
 async def get_total_users():
-    return len(users)
+    return await users_collection.count_documents({})
 
+
+# TOTAL CHATS
 
 async def get_total_chats():
-    return len(chats)
+    return await chats_collection.count_documents({})
