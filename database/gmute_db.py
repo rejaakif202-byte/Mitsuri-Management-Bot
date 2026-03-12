9254dd43db1afca3dd1ccce475fabe41c29ad264
rejaakif202-byte <rejaakif202@gmail.com>
2026-03-13 05:17:34 +0530

    Update gmute_db.py

--- database/gmute_db.py
+++ database/gmute_db.py
@@ -1,24 +1,53 @@
-gmute_users = set()
+from motor.motor_asyncio import AsyncIOMotorClient
+from config import MONGO_URL, DATABASE_NAME
 
+mongo = AsyncIOMotorClient(MONGO_URL)
+db = mongo[DATABASE_NAME]
+
+gmute = db["gmute_users"]
+
+
+# ADD GMUTE
 
 async def add_gmute(user_id):
 
-    gmute_users.add(user_id)
+    data = await gmute.find_one({"user_id": user_id})
 
+    if not data:
+        await gmute.insert_one({
+            "user_id": user_id
+        })
+
+
+# REMOVE GMUTE
 
 async def remove_gmute(user_id):
 
-    gmute_users.discard(user_id)
+    await gmute.delete_one({
+        "user_id": user_id
+    })
 
+
+# CHECK GMUTE
 
 async def is_gmuted(user_id):
 
-    if user_id in gmute_users:
-        return True
+    data = await gmute.find_one({
+        "user_id": user_id
+    })
 
-    return False
+    return bool(data)
 
+
+# GET ALL GMUTES
 
 async def get_gmutes():
 
-    return list(gmute_users)
+    data = gmute.find()
+
+    users = []
+
+    async for user in data:
+        users.append(user["user_id"])
+
+    return users
