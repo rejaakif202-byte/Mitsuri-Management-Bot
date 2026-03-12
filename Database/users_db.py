6ac9bfa86775485c447be840a48635b2aa40e469
rejaakif202-byte <rejaakif202@gmail.com>
2026-03-12 14:49:28 +0530

    Update users_db.py

--- Database/users_db.py
+++ Database/users_db.py
@@ -1,11 +1,22 @@
 users = set()
+chats = set()
 
 
 async def add_user(user_id):
-
     users.add(user_id)
 
 
-async def get_users():
+async def add_chat(chat_id):
+    chats.add(chat_id)
 
+
+async def get_users():
     return list(users)
+
+
+async def get_total_users():
+    return len(users)
+
+
+async def get_total_chats():
+    return len(chats)
