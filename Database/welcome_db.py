6202cd9cb2d88276285af484a1bbf330caf763c6
rejaakif202-byte <rejaakif202@gmail.com>
2026-03-12 15:43:26 +0530

    Update welcome_db.py

--- Database/welcome_db.py
+++ Database/welcome_db.py
@@ -4,7 +4,10 @@ welcome_db = {}
 async def toggle_welcome(chat_id, state):
 
     if chat_id not in welcome_db:
-        welcome_db[chat_id] = {}
+        welcome_db[chat_id] = {
+            "enabled": True,
+            "text": "hey there {first_name}, welcome to {Chatname}. How are you!?"
+        }
 
     welcome_db[chat_id]["enabled"] = state
 
@@ -12,7 +15,10 @@ async def toggle_welcome(chat_id, state):
 async def set_welcome(chat_id, message):
 
     if chat_id not in welcome_db:
-        welcome_db[chat_id] = {}
+        welcome_db[chat_id] = {
+            "enabled": True,
+            "text": ""
+        }
 
     welcome_db[chat_id]["text"] = message.text or message.caption
 
