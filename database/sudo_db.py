c7bf652d856e513132389a3072393399d50abf41
rejaakif202-byte <rejaakif202@gmail.com>
2026-03-13 05:20:43 +0530

    Update sudo_db.py

--- database/sudo_db.py
+++ database/sudo_db.py
@@ -1,42 +1,66 @@
-OWNER_ID = 7846306818
+from motor.motor_asyncio import AsyncIOMotorClient
+from config import MONGO_URL, OWNER_ID
 
-sudo_users = set()
+mongo = AsyncIOMotorClient(MONGO_URL)
+db = mongo["management"]
 
+sudo_db = db["sudo_users"]
+
+
+# ADD SUDO
 
 async def add_sudo(user_id):
 
-    sudo_users.add(user_id)
+    data = await sudo_db.find_one({"user_id": user_id})
 
+    if not data:
+        await sudo_db.insert_one({
+            "user_id": user_id
+        })
+
+
+# REMOVE SUDO
 
 async def remove_sudo(user_id):
 
-    sudo_users.discard(user_id)
+    await sudo_db.delete_one({
+        "user_id": user_id
+    })
 
+
+# GET ALL SUDOS
 
 async def get_sudos():
 
-    return list(sudo_users)
+    users = []
 
+    async for user in sudo_db.find():
+        users.append(user["user_id"])
+
+    return users
+
+
+# OWNER CHECK
 
 async def is_owner(user_id):
 
     return user_id == OWNER_ID
 
 
+# SUDO CHECK
+
 async def is_sudo(user_id):
 
     if user_id == OWNER_ID:
         return True
 
-    if user_id in sudo_users:
-        return True
+    data = await sudo_db.find_one({"user_id": user_id})
 
-    return False
+    return bool(data)
 
+
+# BROADCAST PERMISSION
 
 async def is_broadcast_allowed(user_id):
 
-    if user_id == OWNER_ID:
-        return True
-
-    return False
+    return user_id == OWNER_ID
