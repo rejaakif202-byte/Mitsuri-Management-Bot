f7e677e93aeb98309443852daaa261d7f4f74698
rejaakif202-byte <rejaakif202@gmail.com>
2026-03-13 05:16:50 +0530

    Update gban_db.py

--- database/gban_db.py
+++ database/gban_db.py
@@ -1,24 +1,53 @@
-gban_users = set()
+from motor.motor_asyncio import AsyncIOMotorClient
+from config import MONGO_URL, DATABASE_NAME
 
+mongo = AsyncIOMotorClient(MONGO_URL)
+db = mongo[DATABASE_NAME]
+
+gban = db["gban_users"]
+
+
+# ADD GBAN
 
 async def add_gban(user_id):
 
-    gban_users.add(user_id)
+    data = await gban.find_one({"user_id": user_id})
 
+    if not data:
+        await gban.insert_one({
+            "user_id": user_id
+        })
+
+
+# REMOVE GBAN
 
 async def remove_gban(user_id):
 
-    gban_users.discard(user_id)
+    await gban.delete_one({
+        "user_id": user_id
+    })
 
+
+# CHECK GBAN
 
 async def is_gbanned(user_id):
 
-    if user_id in gban_users:
-        return True
+    data = await gban.find_one({
+        "user_id": user_id
+    })
 
-    return False
+    return bool(data)
 
+
+# GET ALL GBANS
 
 async def get_gbans():
 
-    return list(gban_users)
+    data = gban.find()
+
+    users = []
+
+    async for user in data:
+        users.append(user["user_id"])
+
+    return users
