22c313d0a8d7beacd9307108591ca6cd5cc372fa
rejaakif202-byte <rejaakif202@gmail.com>
2026-03-12 14:53:57 +0530

    Update users_savers.py

--- modules/users_savers.py
+++ modules/users_savers.py
@@ -1,11 +1,19 @@
 from pyrogram import Client, filters
 
-from database.users_db import add_user
+from database.users_db import add_user, add_chat
 
 
 @Client.on_message(filters.private)
-async def save_user(client, message):
+async def save_private_user(client, message):
 
     if message.from_user:
-
         await add_user(message.from_user.id)
+
+
+@Client.on_message(filters.group)
+async def save_group_user(client, message):
+
+    if message.from_user:
+        await add_user(message.from_user.id)
+
+    await add_chat(message.chat.id)
